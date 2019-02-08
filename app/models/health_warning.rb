682227b78a8ef5c61efba567b38581ad68082654
JerYoMat <jeremy.emata@gmail.com>
2019-02-08 00:35:42 -0500

    added model validations

--- app/models/health_warning.rb
+++ app/models/health_warning.rb
@@ -1,2 +1,10 @@
 class HealthWarning < ApplicationRecord
+has_many :ratings 
+belongs_to :user 
+belongs_to :product 
+validates :name, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}  
+validates :link, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}
+validates_format_of :link, :with => %r{\.(png|jpg|jpeg)\z}i
+
+
 end
