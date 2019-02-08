fdaa6618ea5ec8d3676cacf077a2cb653f345360
JerYoMat <jeremy.emata@gmail.com>
2019-02-08 00:59:04 -0500

    added product model tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,12 +3,27 @@ require 'test_helper'
 class ProductTest < ActiveSupport::TestCase
  setup do 
    @product = products(:cigs) 
+   @new_product = Product.new(name: "New Product", logo_path: "something.png")
  end 
 
  teardown do 
    Rails.cache.clear
  end 
+ 
+ test 'should be valid' do 
+   assert @product.valid?
+ end 
 
+ test 'requires name' do 
+   @product.name = nil 
+   assert_not @product.valid?
+ end 
+
+ test 'require logo_path' do
+  @product.logo_path = nil 
+  assert_not @product.valid?
+ end 
+ 
  test 'valid logo filetypes are accepted' do 
   valid_logos = %w[image.jpg different.jpeg anothersomething.png lastone.gif]
   valid_logos.each do |single_address|
@@ -23,5 +38,15 @@ test 'invalid logo filetypes are accepted' do
     @product.logo_path = single_address
     assert_not @product.valid?
   end 
+end 
+
+test 'product name is unique' do 
+  @new_product.name = @product.name.upcase 
+  assert_not @new_product.valid? 
+end 
+
+test 'logo path is unique' do 
+  @new_product.logo_path = @product.logo_path.upcase 
+  assert_not @new_product.valid? 
 end 
 end
