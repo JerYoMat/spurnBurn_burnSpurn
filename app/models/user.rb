41159634539b6ccae5d39b762d2bce87998ad42d
JerYoMat <jeremy.emata@gmail.com>
2019-02-07 20:41:38 -0500

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :ratings 
+    has_many :side_effects, through: :ratings 
+    has_many :side_effects 
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates_presence_of :name, :email
+    validates :name, length: {maximum: 255}
+    validates :email, length: {maximum: 255}, uniqueness: {case_sensitive: false}, format: { with: EMAIL_REGEX}
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    validates :smoking_status, inclusion: { in: ['regular', 'occasional', 'former', 'never', 'none of your business' ] }
 end
