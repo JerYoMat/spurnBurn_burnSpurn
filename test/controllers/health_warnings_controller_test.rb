9259e90e7a6b9d16bc921d3be8719e7064b59cfa
JerYoMat <jeremy.emata@gmail.com>
2019-02-09 06:53:06 -0500

    customized health warning tests

--- test/controllers/health_warnings_controller_test.rb
+++ test/controllers/health_warnings_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class HealthWarningsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @health_warning = health_warnings(:one)
+    @health_warning = health_warnings(:gnarly_foot)
+    @user = users(:smokey_steve)
+    @other_user = users(:kyle_the_prude)
   end
 
   test "should get index" do
@@ -15,34 +17,62 @@ class HealthWarningsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create health_warning" do
+  test "logged in user can create" do
+    log_in_as_t(@user)
     assert_difference('HealthWarning.count') do
-      post health_warnings_url, params: { health_warning: { link: @health_warning.link, name: @health_warning.name } }
+      post health_warnings_url, params: { health_warning: { link: "valid.jpg", name: "new_name" } }
     end
-
     assert_redirected_to health_warning_url(HealthWarning.last)
   end
+
+  test 'unknown user cannot create' do 
+    assert_no_difference('HealthWarning.count') do
+      post health_warnings_url, params: { health_warning: { link: "valid.jpg", name: "new_name" } }
+    end
+    assert_redirected_to root_path
+  end 
 
   test "should show health_warning" do
     get health_warning_url(@health_warning)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "owning user can edit" do
+    log_in_as_t(@health_warning.user)
     get edit_health_warning_url(@health_warning)
     assert_response :success
   end
+  test "different user cannot edit" do
+    log_in_as_t(@other_user)
+    get edit_health_warning_url(@health_warning)
+    assert_redirected_to root_path
+  end
 
-  test "should update health_warning" do
+  test "owner can update health_warning" do
+    log_in_as_t(@health_warning.user)
     patch health_warning_url(@health_warning), params: { health_warning: { link: @health_warning.link, name: @health_warning.name } }
     assert_redirected_to health_warning_url(@health_warning)
   end
 
-  test "should destroy health_warning" do
+  test "different user cannot update health_warning" do
+    log_in_as_t(@other_user)
+    patch health_warning_url(@health_warning), params: { health_warning: { link: @health_warning.link, name: "you shouldn't see me " } }
+    assert_not HealthWarning.find(@health_warning.id).name == "you shouldn't see me "
+  end
+
+
+  test "owner can destroy health_warning" do
+    log_in_as_t(@health_warning.user)
     assert_difference('HealthWarning.count', -1) do
       delete health_warning_url(@health_warning)
     end
-
     assert_redirected_to health_warnings_url
   end
+
+  test "different user cannot destroy health_warning" do
+    assert_no_difference('HealthWarning.count') do
+      delete health_warning_url(@health_warning)
+    end
+  end
+
 end
