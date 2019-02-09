2f21778656f202e96fab4f3dc16e4bda95acbca9
JerYoMat <jeremy.emata@gmail.com>
2019-02-09 06:16:55 -0500

    added ApplicationHelper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,8 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  def log_in_as(user, password: 'password')
+  include ApplicationHelper
+  def log_in_as_t(user, password: 'password')
     post login_path, params: { session: { email: user.email,
                                           password: password } }
   end
