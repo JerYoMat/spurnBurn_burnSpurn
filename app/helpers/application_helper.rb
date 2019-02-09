2ee22f997588f2e4df3319635bb3bd99b2d5cd66
JerYoMat <jeremy.emata@gmail.com>
2019-02-09 06:18:23 -0500

    added app helpers to DRY authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end   
+  
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+        if session[:user_id]
+          @current_user ||= User.find_by(id: session[:user_id])  # Same as @current_user = @current_user || User.find_by(id: session[:user_id])
+        end 
+      end
+    
+      def owning_user?
+      return false if !current_user 
+           if @user.id == @current_user.id 
+            true 
+            else 
+              false 
+            end 
+          end 
+    
+      def logged_in?
+         !current_user.nil?
+      end 
+      
+      def log_out
+        session.delete(:user_id)
+        @current_user = nil
+      end
 end
