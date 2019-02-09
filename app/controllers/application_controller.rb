3e9150928b121caff901adb7d2511792fa86320b
JerYoMat <jeremy.emata@gmail.com>
2019-02-09 07:42:50 -0500

    moved set_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include ApplicationHelper
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
