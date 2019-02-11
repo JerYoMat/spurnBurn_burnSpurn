130faa954baefa45dc87b5ac0869dece09f50e2f
JerYoMat <jeremy.emata@gmail.com>
2019-02-11 04:46:53 -0500

    added test user for manual testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create!(name:  "test user",
+  email: "test@test.com",
+  password:              "password",
+  password_confirmation: "password")
+
 
 100.times do |n|
     name  = Faker::Name.name
