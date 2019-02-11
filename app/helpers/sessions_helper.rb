f7d70d63d60cca8002a173c02adb0c27036287ff
JerYoMat <jeremy.emata@gmail.com>
2019-02-11 00:01:14 -0500

    removed helper methods needed for remember me functionality which is not part of this project

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,7 @@
-module SessionsHelper
+
     module SessionsHelper
         def log_in(user)
             session[:user_id] = user.id
-          end
-    
-          def remember(user)
-            user.remember
-            cookies.permanent.signed[:user_id] = user.id
-            cookies.permanent[:remember_token] = user.remember_token
-          end
-    
-          def forget(user)
-            user.forget
-            cookies.delete(:user_id)
-            cookies.delete(:remember_token)
           end
     
           def current_user
@@ -56,4 +44,3 @@ module SessionsHelper
     
         end
     
-end
